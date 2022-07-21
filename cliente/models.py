bb703b064031fdc9e3f169e9100663fe366ac0f4
Alex <alex.afortiori@gmail.com>
2022-07-21 13:20:05 -0300

    Iniciando modelagem do cliente

--- cliente/models.py
+++ cliente/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+    class Cliente(models.Model):
+        nome = models.CharField(max_length=30)
+        endereco = models.CharField(max_length=250), verbose_name="endereço" #logradouro e número
+        bairro = models.CharField(max_length=150)
+        cidade = models.CharField(max_length=150)
+        estado = models.CharField(max_length=2)
+        cep = models.CharField(max_length=9)
+        ponto_de_referencia = models.CharField(max_length=150)
+        cpf = models.CharField(max_length=14)
+        telefone = models.CharField(max_length=10)
